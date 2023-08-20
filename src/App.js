f718136e9cb3738effe85e2846250f09b1ad0ef4
Neillcllghn <>
2023-08-20 10:23:02 +0000

    Setting up the categories list page to display the categories to user

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SignUpForm from './pages/auth/SignUpForm';
 import LoginForm from './pages/auth/LoginForm';
 import CategoryCreateForm from './pages/categories/CategoryCreateForm';
 import CategoryList from './pages/categories/CategoryList';
-
+import CategoriesList from './pages/categories/CategoriesList';
 
 function App() {
   return (
@@ -20,6 +20,7 @@ function App() {
             <Route exact path="/signup" render={() => <SignUpForm />} />
             <Route exact path="/category/create" render={() => <CategoryCreateForm />} />
             <Route exact path="/category/:id" render={() => <CategoryList />} />
+            <Route exact path="/categorieslist/" render={() => <CategoriesList message="No Results found..."/>} />
             <Route render={() => <p>Page not found!</p>} />
         </Switch>
     </Container>
---
+++ src/pages/categories/CategoriesList.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { axiosReq } from '../../api/axiosDefaults';
+
+import appStyles from "../../App.module.css"
+import Asset from '../../components/Assets';
+import Categories from './Categories';
+
+
+
+function CategoriesList({message, filter=""}) {
+    const [category, setCategory] = useState({ results: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        const fetchCategory = async () => {
+            try {
+                const {data} = await axiosReq.get(`/category/${filter}`)
+                setCategory(data)
+                setHasLoaded(true)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+
+        setHasLoaded(false)
+        const timer = setTimeout(() => {
+            fetchCategory();
+          }, 500);
+      
+          return () => {
+            clearTimeout(timer);
+          };
+
+    }, [filter, pathname])
+
+  return (
+    <Row className='h-100'>
+        <Col className='py-2 p-0 p-lg-2' lg={8}>
+            <p>List of Categories mobile</p>
+
+
+
+
+            {hasLoaded ? (
+                <>
+                {category.results.length ? (
+                    category.results.map(category => (
+                        <Categories key={category.id} {...category} setCategory={setCategory} />
+                    ))
+                ) : (
+                    <Container className={appStyles.Content}>
+                        <Asset message={message} />
+                    </Container>
+                )}
+                </>
+            ) : (
+                <Container className={appStyles.Content}>
+                    <Asset spinner />                     
+                    </Container>
+            )}
+        </Col>
+        <Col className='d-none d-lg-block p-0 p-lg-2' md={4}>
+            <p>List of Categories desktop</p>
+        </Col>
+    </Row>
+  )
+}
+
+export default CategoriesList;
